c9427c2a53e1bab9ce83dbb18a7a6f3613d3dd6c
Fernando Godoy M <fergodoyma4@gmail.com>
2025-04-26 18:56:00 -0400

    Se añadieron mejoras en el codigo del tercer ejercicio

--- Prueba1/ejercicio3.sql
+++ Prueba1/ejercicio3.sql
@@ -22,7 +22,9 @@ DECLARE
 	v_cliente_nombre Clientes.Nombre%TYPE;
 	v_suma_pedidos Pedidos.Total%TYPE;
 
- BEGIN
+	v_contador NUMBER := 0;
+
+BEGIN
  	OPEN c_clientes;
 
 	DBMS_OUTPUT.PUT_LINE('---------SE EMPIEZA A LISTAR--------');
@@ -31,7 +33,22 @@ DECLARE
  		FETCH c_clientes INTO v_cliente_id, v_cliente_nombre, v_suma_pedidos;
  		EXIT WHEN c_clientes%NOTFOUND;
  		DBMS_OUTPUT.PUT_LINE('ClienteID: ' || v_cliente_id || ', Nombre Cliente: ' || v_cliente_nombre || ', Suma Pedidos: ' || v_suma_pedidos);
+ 		v_contador := v_contador + 1;
  	END LOOP;
+
+ 	IF v_contador = 0 THEN
+ 		DBMS_OUTPUT.PUT_LINE('NO SE ENCONTRARON CLIENTES CON DICHA CONDICION');
+ 	END IF;
+
  	CLOSE c_clientes;
+
+EXCEPTION
+	WHEN OTHERS THEN
+ 	DBMS_OUTPUT.PUT_LINE('Error al listar clientes: ' || SQLERRM);
+ 	
+ 	IF c_clientes%ISOPEN THEN
+ 		CLOSE c_clientes;
+ 	END IF;
+
 END;
 /
