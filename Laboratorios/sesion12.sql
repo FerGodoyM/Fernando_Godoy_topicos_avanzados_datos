a35b3693e34c18d3cf07a23dc33d9cf6c0e0e456
Fernando Godoy M <fergodoyma4@gmail.com>
2025-06-08 22:13:09 -0400

    etc/ Editado sesion12 con motivos de estudio

--- Laboratorios/sesion12.sql
+++ Laboratorios/sesion12.sql
@@ -1,3 +1,59 @@
+
+-- Triggers
+
+-- bloque PL/SQL que se ejecuta automáticamente en respuesta a un evento DML 
+-- o DDL en una tabla.
+
+-------------------------EJEMPLOS-------------------------------
+
+-- Trigger para validar el total de un pedido antes de insertarlo.
+
+CREATE OR REPLACE TRIGGER validar_total_pedido
+BEFORE INSERT ON Pedidos
+FOR EACH ROW
+    BEGIN
+        IF :NEW.Total <= 0 THEN
+        RAISE_APPLICATION_ERROR(-20003, 'El total del pedido debe ser mayor a 0.');
+        END IF;
+    END;
+    /
+
+-- Probar el trigger
+INSERT INTO Pedidos (PedidoID, ClienteID, Total, FechaPedido)
+VALUES (105, 3, -100, SYSDATE);
+INSERT INTO Pedidos (PedidoID, ClienteID, Total, FechaPedido)
+VALUES (105, 3, 500, SYSDATE);
+
+
+
+-- Trigger para registrar cambios en el precio de productos en una tabla de auditoría.
+
+-- Crear tabla de auditoría
+CREATE TABLE AuditoriaPrecios (
+    AuditoriaID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ProductoID NUMBER,
+    PrecioAntiguo NUMBER,
+    PrecioNuevo NUMBER,
+    FechaCambio DATE);
+
+-- Crear trigger
+CREATE OR REPLACE TRIGGER auditar_precio_producto
+AFTER UPDATE OF Precio ON Productos
+FOR EACH ROW
+    BEGIN
+        INSERT INTO AuditoriaPrecios (ProductoID, PrecioAntiguo, PrecioNuevo, FechaCambio)
+        VALUES (:OLD.ProductoID, :OLD.Precio, :NEW.Precio, SYSDATE);
+    END;
+    /
+
+-- Probar el trigger
+UPDATE Productos SET Precio = 1300 WHERE ProductoID = 1;
+SELECT * FROM AuditoriaPrecios;
+
+
+
+--------------------------EJERCICIOS---------------------------
+
 -- Crea una función calcular_total_con_descuento que
 -- reciba un PedidoID (parámetro IN) y devuelva el
 -- total del pedido con un descuento del 10% si el
@@ -9,9 +65,7 @@ CREATE OR REPLACE FUNCTION calcular_total_con_descuento(pedido_id IN NUMBER)
 RETURN NUMBER IS
     v_total NUMBER;
 BEGIN
-    SELECT total
-    INTO v_total
-    FROM pedidos
+    SELECT total INTO v_total FROM pedidos
     WHERE id = pedido_id;
 
     IF v_total > 1000 THEN
